658deccf227bdd8d99d5e42f5e12828025228ce9
ssomlk <shankasomasiri@gmail.com>
2021-11-21 21:59:10 +0530

    rempved unwanted code

--- src/components/maincommonlayout/MainCommonLayoutSection.js
+++ src/components/maincommonlayout/MainCommonLayoutSection.js
@@ -28,31 +28,3 @@ function MainCommonLayoutSection({title, description, photo}){
 
 export default MainCommonLayoutSection;
 
-// const MainCommonLayoutSection = () => {
-//   const renderMainLayoutSection = () => {
-//     return (
-//     <div className={styles.customAboutUsEllipse}>
-//       <div className="container">
-//         <div className="row">
-//             <div className="col">
-//               <div className={styles.mainTitle}>About Us</div>
-//               <div className={styles.mainBody}>
-//                 Comprehensive training institute aimed at facilitating spiritual and academic development of Buddhist monks and nuns, based on Theravada teachings.</div>
-//               </div>
-//             <div className="col">
-//               <img className={styles.mainImg} src="/monk-hike-deep-forest-reflection-with-lake-buddha-religion-concept.png"/>
-//             </div>
-//         </div>
-//       </div>
-//     </div>
-//     );
-//   };
-
-//   return (
-//     <Container className={styles.customAboutUsCarouselWrapper}>
-//       {renderMainLayoutSection()}
-//     </Container>
-//   );
-// };
-// export default MainCommonLayoutSection;
-

0a916aef76690cbe8471f7791f6ea42e23a5859f
Milinda Kalyanawansa <milindaka@millenniumitesp.com>
2021-11-23 18:52:50 +0530

    Added testomonials

---
+++ src/components/testomonials/Testomonials.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Container, Carousel, Row, Col } from "react-bootstrap";
+import data from '../../data/testomonialsData.json'
+import styles from './Testomonials.module.css';
+import ReactPlayer from 'react-player';
+import {BsFillChatRightQuoteFill} from 'react-icons/bs';
+
+const Testomonials = () => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [playLabel, setPlayLable] = useState("Play")
+
+    const handlePlaying = () => {
+         if(isPlaying) {
+            setIsPlaying(false);
+            setPlayLable("Play");
+         } else{
+            setIsPlaying(true);
+            setPlayLable("Stop");
+         } 
+    }
+
+  const renderCarousel = (item, index) => {
+    return (
+      
+        <Carousel.Item key={index}>
+         <Row>
+             <Col>
+             <ReactPlayer url={item.video}  controle={true}/>
+             </Col>
+             <Col>
+             <img src={item.image}/>
+
+             <div style={{textAlign: 'left;', font: 'normal normal normal 41px/49px Raleway',letterSpacing: '0px',color: '#FFFFFF', 
+             opacity: '0.73'}}><BsFillChatRightQuoteFill/> &nbsp;{item.quote}</div>
+
+             <div style={{color: '#D1C7BB',textAlign: 'left', font: 'normal normal 600 26px/61px Raleway', letterSpacing: '0px', opacity: '0.73'
+             }}>{item.author}</div>
+
+                <div style={{color: '#FFFFFF',textAlign: 'left', font: 'normal normal 600 14px/19px Raleway', letterSpacing: '0px', opacity: '0.38'
+             }}>{item.description}</div>
+             </Col>
+         </Row>
+        </Carousel.Item>
+
+    );
+  };
+
+  return (
+      <Carousel style={{background:'#532F00', width:'100%', height:'100%'}}>
+
+    {data.map(renderCarousel)}   
+    </Carousel>
+       );
+};
+export default Testomonials;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Header from "../components/header/Header";
 import NewsAndEventsComponent from "../components/newsandevents/NewsAndEventsComponent";
 import Footer from "../components/footer/Footer";
 import GeneralFAQ from "../components/generalfaq/GeneralFAQ";
+import Testomonials from "../components/testomonials/Testomonials";
 
 
 export default function Home() {
@@ -29,6 +30,7 @@ export default function Home() {
       
       <CardDeckComponent/>
        <NewsAndEventsComponent/>
+      <Testomonials/>
       <GeneralFAQ/>
       <Footer/> 
     </div>

def2f74df89e1aa6b6b3b1aecc83652a06c6f0fd
ssomlk <shankasomasiri@gmail.com>
2021-11-23 14:31:11 +0530

    add components to contact us

---
+++ src/components/contactus/ContactUs.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+function ContactUs() {  
+    return (
+        <div className="contactDataWrapper">
+        <div className="contactLeftContainer paddingHorizontal30">
+            <div className="nameContainer">
+                <div className="lineHeight1">International Institute of</div>
+                <div className="lineHeight1">Terawada</div>
+            </div>
+            <div className="addressContainer mb-25 pl-20">
+                <div className="addressImageCont mr-24">
+                    <img
+                        className="d-block w-100 pin-img-max-width"
+                        src="/Address.svg"
+                        alt="Address.svg"
+                        width="24px"
+                    />
+                </div>
+                <div className="addressNameContainer">
+                    <div>
+                        Hinetikalma Watta Road
+                    </div>
+                    <div>
+                        Kaluwaragas wewa
+                    </div>
+                    <div>
+                        Sri Lanka
+                    </div>
+                </div>
+            </div>
+            <div className="commMethodContainer mb-25 pl-20">
+                <div className="contactImageCont mr-18">
+                    <img
+                        className="d-block w-100 contact-img-max-width"
+                        src="/contact.svg"
+                        alt="Contact.svg"
+                        width="36px"
+                    />
+                </div>
+                <div className="contactDetailsContainer">
+                    <div>
+                        info@iit.lk
+                    </div>
+                    <div>
+                        +94 112 897 8899
+                    </div>
+                </div>
+            </div>
+            <div className="directionsContainer mb-25 pl-20">
+                <div className="directionsImageCont mr-24">
+                    <img
+                        className="d-block w-100 pin-img-max-width"
+                        src="/Directions.svg"
+                        alt="Directions.svg"
+                        width="24px"
+                    />
+                </div>
+                <div className="directionDetailsContainer">
+                    <a>Get Directions</a>
+                </div>
+            </div>
+        </div>
+        <div className="contactRightContainer paddingHorizontal30">
+            <form className="contactUsForm">
+                <div className="mb-26 flex">
+                    <div className="mr-32">
+                        <label htmlFor="contactName" className="form-label contactLabelsText">Name</label>
+                        <input type="text" className="form-control width-356" id="contactName"/>
+                    </div>
+                    <div>
+                        <label htmlFor="contactEmail" className="form-label contactLabelsText">Email</label>
+                        <input type="email" className="form-control width-356" id="contactEmail" placeholder="name@example.com"/>
+                    </div>
+                </div>
+                <div className="mb-26 flex">
+                    <div className="mr-32">
+                        <label htmlFor="contactPhone" className="form-label contactLabelsText">Phone</label>
+                        <input type="text" className="form-control width-356" id="contactPhone"/>
+                    </div>
+                    <div>
+                        <label htmlFor="contactSubject" className="form-label contactLabelsText">Subject</label>
+                        <input type="text" className="form-control width-356" id="contactSubject"/>
+                    </div>
+                </div>
+                <div className="mb-26">
+                    <label htmlFor="contactMessage" className="form-label contactLabelsText">Message</label>
+                    <textarea className="form-control min-height-213" id="contactMessage" rows="3"></textarea>
+                </div>
+                <div className="mb-26">
+                    <button type="button" className="submitButton">SUBMIT</button>
+                </div>
+            </form>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ContactUs;
--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -4,11 +4,7 @@ import { useRouter } from "next/router";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import MainLayoutSection from "../components/maincommonlayout/MainCommonLayoutSection";
-import NavigationCommonLayout  from "../components/navigationcommonlayout/NavigationCommonLayout";
-import { Container, Row, Col } from "react-bootstrap";
-import {  BsFillArrowRightCircleFill } from "react-icons/bs";
-import OurFocusCardDeck from "../components/ourfocuscarddeck/OurFocusCardDeck";
-import { aboutUsList } from "../data/commonLayoutNavigationData";
+import ContactUs from "../components/contactus/ContactUs";
 
 export default function AboutUs() {
   const { t, lang } = useTranslation();
@@ -33,97 +29,7 @@ export default function AboutUs() {
         photo="/monk-hike-deep-forest-reflection-with-lake-buddha-religion-concept.png"
       /> 
 
-      <div className="contactDataWrapper">
-        <div className="contactLeftContainer paddingHorizontal30">
-            <div className="nameContainer">
-                <div className="lineHeight1">International Institute of</div>
-                <div className="lineHeight1">Terawada</div>
-            </div>
-            <div className="addressContainer mb-25 pl-20">
-                <div className="addressImageCont mr-24">
-                    <img
-                        className="d-block w-100 pin-img-max-width"
-                        src="/Address.svg"
-                        alt="Address.svg"
-                        width="24px"
-                    />
-                </div>
-                <div className="addressNameContainer">
-                    <div>
-                        Hinetikalma Watta Road
-                    </div>
-                    <div>
-                        Kaluwaragas wewa
-                    </div>
-                    <div>
-                        Sri Lanka
-                    </div>
-                </div>
-            </div>
-            <div className="commMethodContainer mb-25 pl-20">
-                <div className="contactImageCont mr-18">
-                    <img
-                        className="d-block w-100 contact-img-max-width"
-                        src="/contact.svg"
-                        alt="Contact.svg"
-                        width="36px"
-                    />
-                </div>
-                <div className="contactDetailsContainer">
-                    <div>
-                        info@iit.lk
-                    </div>
-                    <div>
-                        +94 112 897 8899
-                    </div>
-                </div>
-            </div>
-            <div className="directionsContainer mb-25 pl-20">
-                <div className="directionsImageCont mr-24">
-                    <img
-                        className="d-block w-100 pin-img-max-width"
-                        src="/Directions.svg"
-                        alt="Directions.svg"
-                        width="24px"
-                    />
-                </div>
-                <div className="directionDetailsContainer">
-                    <a>Get Directions</a>
-                </div>
-            </div>
-        </div>
-        <div className="contactRightContainer paddingHorizontal30">
-            <form className="contactUsForm">
-                <div className="mb-26 flex">
-                    <div className="mr-32">
-                        <label htmlFor="contactName" className="form-label contactLabelsText">Name</label>
-                        <input type="text" className="form-control width-356" id="contactName"/>
-                    </div>
-                    <div>
-                        <label htmlFor="contactEmail" className="form-label contactLabelsText">Email</label>
-                        <input type="email" className="form-control width-356" id="contactEmail" placeholder="name@example.com"/>
-                    </div>
-                </div>
-                <div className="mb-26 flex">
-                    <div className="mr-32">
-                        <label htmlFor="contactPhone" className="form-label contactLabelsText">Phone</label>
-                        <input type="text" className="form-control width-356" id="contactPhone"/>
-                    </div>
-                    <div>
-                        <label htmlFor="contactSubject" className="form-label contactLabelsText">Subject</label>
-                        <input type="text" className="form-control width-356" id="contactSubject"/>
-                    </div>
-                </div>
-                <div className="mb-26">
-                    <label htmlFor="contactMessage" className="form-label contactLabelsText">Message</label>
-                    <textarea className="form-control min-height-213" id="contactMessage" rows="3"></textarea>
-                </div>
-                <div className="mb-26">
-                    <button type="button" class="submitButton">SUBMIT</button>
-                </div>
-            </form>
-        </div>
-      </div>
+      <ContactUs/>
 
       <Footer/>
     </div>

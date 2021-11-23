12e8639434744c001980d83ee28d16b7599a3a37
ssomlk <shankasomasiri@gmail.com>
2021-11-23 10:26:13 +0530

    Adding contact us page

---
+++ src/pages/contactus.js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import MainLayoutSection from "../components/maincommonlayout/MainCommonLayoutSection";
+import NavigationCommonLayout  from "../components/navigationcommonlayout/NavigationCommonLayout";
+import { Container, Row, Col } from "react-bootstrap";
+import {  BsFillArrowRightCircleFill } from "react-icons/bs";
+import OurFocusCardDeck from "../components/ourfocuscarddeck/OurFocusCardDeck";
+import { aboutUsList } from "../data/commonLayoutNavigationData";
+
+export default function AboutUs() {
+  const { t, lang } = useTranslation();
+  const router = useRouter();
+
+  return (
+    <div className='skeleton'>
+      <Head>
+        <title>Contact Us</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="international institute of theravada" />
+      </Head>
+
+      <div className='navbarCarouselWrapper contact'>
+        <Header />
+      </div>
+
+      <MainLayoutSection 
+        title="Contact Us" 
+        description="Comprehensive training institute aimed at facilitating spiritual and academic development of Buddhist monks and nuns, based on Theravada teachings."
+        photo="/monk-hike-deep-forest-reflection-with-lake-buddha-religion-concept.png"
+      /> 
+
+      <Footer/>
+    </div>
+
+    
+  );
+}
+
+export async function getServerSideProps(context) {
+  // const products = await fetch('https://fakestoreapi.com/products')
+  // .then(res=>res.json());
+
+  return {
+    props: {
+      //products
+    }
+  }
+}

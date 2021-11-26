f16fe15b4cb01ab793717b5bd4627a402c25d598
Rajeev Banda <rajeev.banda@health.vic.gov.au>
2021-11-27 01:11:29 +1100

    Update styles for common header section

--- src/components/maincommonlayout/MainCommonLayoutSection.js
+++ src/components/maincommonlayout/MainCommonLayoutSection.js
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import React from "react";
 import styles from "./MainLayoutSection.module.css";
-import Carousel from 'react-bootstrap/Carousel';
 
 function MainCommonLayoutSection({ title, description, photo, backgroundImg }) {
   return (
     <div className={styles.mainHeader}>
-      <div className={styles.headerBackground} style={{backgroundImage:backgroundImg}}>
-        <div className={styles.carouwrap}>
-          <div className={styles.motoContainer}>
-            <div className='width-50-per'>
-              <img
-                className="d-block w-100 banner-img-max-width-about"
-                src={photo}
-                alt={photo}
-                width="644px"
-              />
-            </div>
-            <Carousel.Caption className='caroucap width-50-per banner-text-left-about-pd'>
+      <div className={styles.headerBackground} style={{ backgroundImage: backgroundImg }}>
+        <div className={styles.headerContainer}>
+          <div className={styles.motoContainer}>           
               <div className={styles.bannerTextOne}>{title}</div>
               <div className={styles.bannerTextTwo}>{description}</div>
-            </Carousel.Caption>
+          </div>
+
+          <div className={styles.headerImageContainer}>
+            <img className={styles.headerImage}
+              src={photo}
+              alt={photo}
+            />
           </div>
         </div>
       </div>

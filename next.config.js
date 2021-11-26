e65447382e2fe8be2edc5b81c51ad91fc4871090
ssomlk <shankasomasiri@gmail.com>
2021-11-26 19:11:38 +0530

    add recaptcha and validation

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
         domains:[]
     },
     env:{
+        RECAPTCHA_SITE_KEY:'6LeW_1kdAAAAANyU93I2sc690ScfYeYgJUxWjLAq',
+        RECAPTCHA_SECRET_KEY:'6LeW_1kdAAAAAPk7Qpikoo5yh1f6RCg9i3SMEBGz',
     },
     ...nextTranslate()
 }

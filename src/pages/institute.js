c1d80cb76193078741ffed0cda55f90a7870e790
Milinda Kalyanawansa <milindaka@millenniumitesp.com>
2021-12-05 02:34:14 +0530

    Added mobile and large screen responsiveness to institute page

--- src/pages/institute.js
+++ src/pages/institute.js
@@ -25,109 +25,162 @@ export default function Institute() {
       photo="/one-buddha-gold-temple-thailand.png"
       backgroundImg="url(/Ellipse-4.svg)"
     /> 
-    <NavigationCommonLayout navigationList={instituteList}/> 
+    {/* <NavigationCommonLayout navigationList={instituteList}/>  */}
 
-<Container>
-<div style={{color: '#5D5D5D',font: 'normal normal bold 50px/59px Raleway',marginBottom:'40px', marginTop:'30px' }}>Academic Staff</div>
-<Row key='R1'>
-{FirstRow.map((item, index) => (
-        <Col md={6} lg={6} key={index +"A"}>
-                <img src={item.image} className='inst-academic-image'/> 
+<Container className='inst-academic-container'>
+<div className='inst-academic-header'>Academic Staff</div>
 
-          <div className='inst-academic-name'>{item.name}</div>
-    <div className='inst-academic-heading'>{item.heading}</div>
-    <div className='inst-academic-body'>{item.body}</div>
-        </Col>
-      ))}
-</Row>
+   <div className='inst-academic-row1-main'>
+      <div className='inst-academic-row1-child1'>
+        <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Werapitiye Dewānanda Thera <div className='inst-academic-heading'>President of the IIT</div> </div>
+     </div>
+    <div  className='inst-academic-body'>BA & MA in Buddha Dhamma at the International Theravāda Buddhist Missionary University, Yangon. BA (Hon) in Sanskrit at the University of Peradeniya, Vidyanidhi Panditha Vidyodaya Parivena, extensive experiences in propagating Buddhist teachings in both Australia and Hawaii.</div>
+    </div>
 
-<Row style={{marginTop:'30px'}} >
+    <div className='inst-academic-row1-child2' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Watagoda Maggavihari Thera <div className='inst-academic-heading'>Vice-President of the IIT</div> </div>
+    </div>
+    <div className='inst-academic-body'>BA & MA in Buddha Dhamma at the International Theravāda Buddhist Missionary University, Yangon. Also undergone monastic training under the Thai Forest Tradition.</div>
+    </div>
 
-{SecondRow.map((item, index) => (
-        <Col md={4} lg={4} key={index + "B"}>
-                <img src={item.image} className='inst-academic-image'/> 
+</div>
 
-                  <div className='inst-academic-name'>{item.name}</div>
-    <div className='inst-academic-heading'>{item.heading}</div>
-    <div className='inst-academic-body'>{item.body}</div>
-           </Col>
-      ))}
+<div className='inst-academic-row2-main'>
 
-</Row>
+<div className='inst-academic-row2-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Matugama Dhammanisanti Thera <div className='inst-academic-heading'></div> </div>
+    </div>
+    <div className='inst-academic-body'>An experienced Abhidhamma teacher currently conducting basic and advanced Abhidhamma courses for lay devotees in Colombo for over five years.</div>
+    </div>
 
-<Row style={{marginTop:'30px'}} >
+    <div className='inst-academic-row2-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Ududigana Siddhatthālaṅkāra Thera<div className='inst-academic-heading'>Secretary of the IIT and Head of the Vinaya Department</div> </div>
+    </div>
+    <div className='inst-academic-body'>BA in Buddha Dhamma at the International Theravāda Buddhist Missionary University, Yangon. The first Sri Lankan to pass all the three stages in both Sāsnālaṅkāra (Sāmaṇejo) and Vinayavidu Traditional Buddhist Examinations in Myanmar.</div>
+    </div>
 
-{ThirdRow.map((item, index) => (
-        <Col md={4} lg={4} key={index + "C"}>
-                <img src={item.image} className='inst-academic-image'/> 
-                <div className='inst-academic-name'>         {item.name}</div>
-    <div className='inst-academic-heading'>{item.heading}</div>
-    <div className='inst-academic-body'>{item.body}</div>
-         </Col>
-      ))}
-</Row>
+    <div className='inst-academic-row2-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Kiribathgoda Tilokānanda Thera<div className='inst-academic-heading'></div> </div>
+    </div>
+    <div className='inst-academic-body'>Diploma in Buddha Dhamma at the International Theravāda Buddhist Missionary University, Yangon. He has undergone monastic training under the Thai Forest Tradition.</div>
+    </div>
 
-<Row style={{marginTop:'30px'}} >
+ </div>
 
-{FourthRow.map((item, index) => (
-        <Col md={6} lg={6} key={index + "D"}>
-                <img src={item.image} className='inst-academic-image'/> 
 
-               <div className='inst-academic-name'>{item.name}</div>
-    <div className='inst-academic-heading'>{item.heading}</div>
-    <div className='inst-academic-body'>{item.body}</div>
-          </Col>
-      ))}
-</Row>
+ <div className='inst-academic-row3-main'>
+
+<div className='inst-academic-row3-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Labunoruwe Dhammavihari Thera <div className='inst-academic-heading'></div> </div>
+    </div>
+    <div className='inst-academic-body'>Passed the Vinayācariya and Abhidhammācariya examinations held under the Srī Kaḷyāṇī Yogāśrama Saṃsthā, Rāmañña Mahānikāya, Sri Lanka. He is currently a Vinaya teacher at the Nā-Uyana Forest Monastery, Sri Lanka.</div>
+    </div>
+
+    <div className='inst-academic-row3-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Walawe Sumedhaloka Thera<div className='inst-academic-heading'>Head of the Language Department</div> </div>
+    </div>
+    <div className='inst-academic-body'>Diploma in Buddha Dhamma at the International Theravāda Buddhist Missionary University, Yangon. He has also earned knowledge and experience under the monastic education system in Myanmar for four years and has gained training under the Thai Forest Tradition as well. He is a competent Abhidhamma- and Pāḷi-teacher capable teaching in both Sinhalese and English.</div>
+    </div>
+
+    <div className='inst-academic-row3-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Deniyaye Upasama Thera<div className='inst-academic-heading'>Head of the Abhidhamma Department</div> </div>
+    </div>
+    <div className='inst-academic-body'>He has studied the Abhidhamma with specialization in the Paṭṭhānappakaraṇa, the seventh and most profound book in the basket of Abhidhamma and had taught Abhidhamma in Burmese language in Myanmar and also in Sinhala at several Sri Lankan monastic education institutes. He is arguably the best academic in the field of Paṭṭhāna studies currently in Sri Lanka.</div>
+    </div>
+
+ </div>
+
+ <div className='inst-academic-row4-main'>
+
+<div className='inst-academic-row4-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Kuliyāpitiye Saṅgharatana Thera<div className='inst-academic-heading'></div> </div>
+    </div>
+    <div className='inst-academic-body'>Diploma in Buddha Dhamma, International Theravāda Buddhist Missionary University, Yangon.</div>
+    </div>
+
+    <div className='inst-academic-row4-child' >
+    <div style={{display:'flex'}}>
+                <img src="profile.png" className='inst-academic-image'/> 
+          <div className='inst-academic-name'>Ven Palmadulle Vijitānandābhivaṃsa Thera<div className='inst-academic-heading'>Vice Secretary of the IIT and Head of the Pāḷi, Sanskrit and Prakrit Department</div> </div>
+    </div>
+    <div className='inst-academic-body'>One of the most academically celebrated Sri Lankan Buddhist monks studied in Myanmar. Successfully completed the first stage of the well-celebrated Sakyasīha Dhammācariaya Examination (known as the Mandalay Abhivaṃsa Examination) becoming the second Sri Lankan to make this achievement. Only about six hundred monks have passed the first level of the Abhivaṃsa Examination, in Myanmar, throughout its history of more than a century. He has also passed one of the three subjects of the second stage of the Sakyasīha Dhammācariya (Abhivaṃsa) Examination. Venerable Vijitānanda has also successfully passed the Sāsanadhaja Dhammācariya Examination in Myanmar. He is currently a Dhamma teacher at the well-celebrated Yuwa Mā Pariyatti Center, Ein Sein, Yangon.</div>
+    </div>
+
+ </div>
 
 </Container>
-{/* 
-<Container>
-<div style={{ color: '#202020', textAlign:'left', font: 'normal normal bold 30px/35px Raleway', letterSpacing:'0px', marginBottom:'25px'}}>Administrational Structure</div>
-<Row key='14'>
+
+<Container fluid className='inst-admin-struct-container'>
+<div className='inst-admin-struct-header'>Administrational Structure</div>
+<Row className='inst-admin-struct-row'>
+
 <Col key='15'>
-<div style={{ color: '#4E4E4E', textAlign:'left', font: 'normal normal bold 42px/49px Raleway', letterSpacing:'0px'}}>IIT as an institution has both monastic features and, at the same time, features of a well organized academic institute. IIT is governed by its General Assembly of which powers are exersiced by three independent bodies, i.e., Executive Board, Senet and Vinaya Council.</div>
-<div style={{ color: '#202020', textAlign:'left', font: 'normal normal 600 24px/36px Raleway', letterSpacing:'0px'}}>IIT is an independent academic institution that would retain autonomy with regard to, all aspects of decision-making.</div>
+<div className='inst-admin-struct-main-text' >IIT as an institution has both monastic features and, at the same time, features of a well organized academic institute. IIT is governed by its General Assembly of which powers are exersiced by three independent bodies, i.e., Executive Board, Senet and Vinaya Council.</div>
+<div className='inst-admin-struct-sub-text'>IIT is an independent academic institution that would retain autonomy with regard to, all aspects of decision-making.</div>
 </Col>
 
-<Col key='16'>
-<Row key='17'>
-  <div style={{color: '#202020', textAlign:'left', font: 'normal normal bold 24px/36px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>General Assembly</div>
-<div style={{color: '#616057', textAlign:'left', font: 'normal normal bold 20px/35px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Executive Board consisting of senior monks who are responsible in governing the IIT. It is divided into two sub-councils, i.e., Study Council and Monastic Council</div>
-</Row>
+<Col className='inst-admin-struct-2nd-col'>
 
-<Row key='18'>
-  <div style={{color: '#202020', textAlign:'left', font: 'normal normal bold 24px/36px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Study Council</div>
-<div style={{color: '#616057', textAlign:'left', font: 'normal normal bold 20px/35px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Study Council is consisting of erudite monks who manage all academic and spriritual activities of the IIT</div>
+
+<Row key='17'>
+  <Col>
+  <div className='inst-admin-struct-col-header'>General Assembly</div>
+<div className='inst-admin-struct-col-body'>Executive Board consisting of senior monks who are responsible in governing the IIT. It is divided into two sub-councils, i.e., Study Council and Monastic Council</div>
+</Col>
+
+<Col>
+  <div className='inst-admin-struct-col-header'>Study Council</div>
+<div className='inst-admin-struct-col-body'>Study Council is consisting of erudite monks who manage all academic and spriritual activities of the IIT</div>
+</Col>
 </Row>
 
 <Row key='19'>
-  <div style={{color: '#202020', textAlign:'left', font: 'normal normal bold 24px/36px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Senat</div>
-<div style={{color: '#616057', textAlign:'left', font: 'normal normal bold 20px/35px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Senet is the body consisting of members of the IIT who are knowlagable in the Theravada scriptures and who observe the decisions taken by the Executive Board and discuss on Dhamma Vinaya issues.</div>
-</Row>
-
+  <Col>
+  <div className='inst-admin-struct-col-header'>Senat</div>
+<div className='inst-admin-struct-col-body' >Senet is the body consisting of members of the IIT who are knowlagable in the Theravada scriptures and who observe the decisions taken by the Executive Board and discuss on Dhamma Vinaya issues.</div>
 </Col>
 
+
 <Col key='20'>
-<Row key='21'>
-  <div style={{color: '#202020', textAlign:'left', font: 'normal normal bold 24px/36px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Executive Board</div>
-<div style={{color: '#616057', textAlign:'left', font: 'normal normal bold 20px/35px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Executive Board consisting of senior monks who are responsible in governing the IIT. It is divided into two sub-councils, i.e., Study Council and Monastic Council</div>
+  <div className='inst-admin-struct-col-header'>Executive Board</div>
+<div className='inst-admin-struct-col-body'>Executive Board consisting of senior monks who are responsible in governing the IIT. It is divided into two sub-councils, i.e., Study Council and Monastic Council</div>
+</Col>
 </Row>
 
 <Row key='22'>
-  <div style={{color: '#202020', textAlign:'left', font: 'normal normal bold 24px/36px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Monsatic Council</div>
-<div style={{color: '#616057', textAlign:'left', font: 'normal normal bold 20px/35px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Monastic Council is cosisiting of skilled monks who supervise the construction of the IIT and the well-being of the residents of the IIT</div>
-</Row>
+  <Col>
+  <div className='inst-admin-struct-col-header'>Monsatic Council</div>
+<div className='inst-admin-struct-col-body'>Monastic Council is cosisiting of skilled monks who supervise the construction of the IIT and the well-being of the residents of the IIT</div>
+</Col>
 
-<Row key='23'>
-  <div style={{color: '#202020', textAlign:'left', font: 'normal normal bold 24px/36px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Vinay Council</div>
-<div style={{color: '#616057', textAlign:'left', font: 'normal normal bold 20px/35px Raleway', letterSpacing:'0px', marginBottom:'15px'}}>Vinaya Council consisting of monks well versed in the monks disciplinary code, and decide on all disciplinary matters of the IIT</div>
+<Col>
+  <div className='inst-admin-struct-col-header'>Vinay Council</div>
+<div className='inst-admin-struct-col-body'>Vinaya Council consisting of monks well versed in the monks disciplinary code, and decide on all disciplinary matters of the IIT</div>
+</Col>
 </Row>
 
 </Col>
 </Row > 
 
-</Container> */}
+</Container>
 
       <Footer/>
       </div>

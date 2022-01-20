72604c9148044b522eaaa657b19bb002a27e6488
Milinda Kalyanawansa <milindaka@millenniumitesp.com>
2022-01-21 00:10:01 +0530

    Added home page data

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,55 +1,86 @@
-import React, { useState, useEffect } from "react";
-import { Container, Carousel, Row, Col } from "react-bootstrap";
-import data from '../../data/testimonialsData.json'
+import React, { useState, useEffect } from 'react';
+import { Container, Carousel, Row, Col } from 'react-bootstrap';
+import data from '../../data/testimonialsData.json';
 import styles from './Testimonials.module.css';
 import ReactPlayer from 'react-player';
-import {BsFillChatRightQuoteFill} from 'react-icons/bs';
+import { BsFillChatRightQuoteFill } from 'react-icons/bs';
 
 const Testomonials = () => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [playLabel, setPlayLable] = useState("Play")
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playLabel, setPlayLable] = useState('Play');
 
-    const handlePlaying = () => {
-         if(isPlaying) {
-            setIsPlaying(false);
-            setPlayLable("Play");
-         } else{
-            setIsPlaying(true);
-            setPlayLable("Stop");
-         } 
+  const handlePlaying = () => {
+    if (isPlaying) {
+      setIsPlaying(false);
+      setPlayLable('Play');
+    } else {
+      setIsPlaying(true);
+      setPlayLable('Stop');
     }
+  };
 
   const renderCarousel = (item, index) => {
     return (
-      
-        <Carousel.Item key={index}>
-         <Row>
-             <Col>
-             <ReactPlayer url={item.video}  control="true"/>
-             </Col>
-             <Col>
-             <img src={item.image}/>
+      <Carousel.Item key={index}>
+        <Row>
+          <Col>
+            <div className="player-wrapper">
+              <ReactPlayer url={item.video} controls={true}  className="react-player"/>
+            </div>
+          </Col>
+          <Col>
+            <img src={item.image} />
 
-             <div style={{textAlign: 'left', font: 'normal normal normal 41px/49px Raleway',letterSpacing: '0px',color: '#FFFFFF', 
-             opacity: '0.73'}}><BsFillChatRightQuoteFill/> &nbsp;{item.quote}</div>
+            <div
+              style={{
+                textAlign: 'left',
+                font: 'normal normal normal 41px/49px Raleway',
+                letterSpacing: '0px',
+                color: '#FFFFFF',
+                opacity: '0.73',
+                padding:'10%'
+              }}
+            >
+            <img
+                className="d-block w-100 banner-social-icon-max-width-45"
+                src="/Path 691.svg"
+                alt="Quote"
+              /> &nbsp;{item.quote}
+            </div>
 
-             <div style={{color: '#D1C7BB',textAlign: 'left', font: 'normal normal 600 26px/61px Raleway', letterSpacing: '0px', opacity: '0.73'
-             }}>{item.author}</div>
+            <div
+              style={{
+                color: '#D1C7BB',
+                textAlign: 'left',
+                font: 'normal normal 600 26px/61px Raleway',
+                letterSpacing: '0px',
+                opacity: '0.73',
+              }}
+            >
+              {item.author}
+            </div>
 
-                <div style={{color: '#FFFFFF',textAlign: 'left', font: 'normal normal 600 14px/19px Raleway', letterSpacing: '0px', opacity: '0.38'
-             }}>{item.description}</div>
-             </Col>
-         </Row>
-        </Carousel.Item>
-
+            <div
+              style={{
+                color: '#FFFFFF',
+                textAlign: 'left',
+                font: 'normal normal 600 14px/19px Raleway',
+                letterSpacing: '0px',
+                opacity: '0.38',
+              }}
+            >
+              {item.description}
+            </div>
+          </Col>
+        </Row>
+      </Carousel.Item>
     );
   };
 
   return (
-      <Carousel style={{background:'#532F00', width:'100%', height:'100%'}}>
-
-    {data.map(renderCarousel)}   
+    <Carousel style={{ background: '#532F00', width: '100%', height: '100%' }}>
+      {data.map(renderCarousel)}
     </Carousel>
-       );
+  );
 };
 export default Testomonials;

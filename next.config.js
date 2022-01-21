2facb0d4b5c67185e1da018d17fc35978865a6fd
ssomlk <shankasomasiri@gmail.com>
2022-01-22 02:15:47 +0530

    added origins

--- next.config.js
+++ next.config.js
@@ -13,7 +13,10 @@ module.exports = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.theravado.com, https://iit-web-app.vercel.app',
+          },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',

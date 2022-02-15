6a74096e22b55ce5a43d85e0e3eda5dac3d26bb9
ssomlk <shankasomasiri@gmail.com>
2022-02-15 08:51:53 +0530

    changes 1

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ module.exports = {
   images: {
     domains: [],
   },
-  env: {},
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
+    SENDGRID_MAIL_RECEIVER: process.env.SENDGRID_MAIL_RECEIVER,
+    SENDGRID_MAIL_SENDER: process.env.SENDGRID_MAIL_SENDER,
+  },
   ...nextTranslate(),
 };
